c94637091e1d6158012e3604a83ac2cb053df9de
Dinnertable <dinnertable@hotmail.ca>
2019-02-25 00:37:41 -0500

    Use consistent naming convention

--- AutoMark.lua
+++ AutoMark.lua
@@ -5,26 +5,25 @@ function shuffle(array)
    
    -- fisher-yates
    local output = { }
-   local random = math.random
    
    for index = 1, #array do
       local offset = index - 1
       local value = array[index]
-      local randomIndex = offset*random()
-      local flooredIndex = randomIndex - randomIndex%1
+      local random_index = offset * math.random()
+      local floored_index = random_index - random_index % 1
       
-      if flooredIndex == offset then
+      if floored_index == offset then
          output[#output + 1] = value
       else
-         output[#output + 1] = output[flooredIndex + 1]
-         output[flooredIndex + 1] = value
+         output[#output + 1] = output[floored_index + 1]
+         output[floored_index + 1] = value
       end
    end
    
    return output
 end
 
-local function table_contains (tab, value)
+local function table_contains(tab, value)
    for _, v in ipairs(tab) do
       if value == v then
          return true
